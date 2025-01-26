4da1d063562e3d39b1b0864c70ec3324079e5f01
Time Pot <9tamagot@windowslive.com>
2025-01-26 16:37:51 +0700

    Change .env localhost  url to real backend

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(compression({
 }));
 
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://slime-list-todolist-app-fullstack.onrender.com',
   methods: ['GET','POST','PUT','PATCH','DELETE'],
   credentials: true,
  
