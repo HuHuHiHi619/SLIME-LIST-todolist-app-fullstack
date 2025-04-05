8084cc15d852fdc5ab00ccce74cce547c26ad1e3
Time Pot <9tamagot@windowslive.com>
2025-04-05 15:16:53 +0700

    Fix dependencies

--- server/server.js
+++ server/server.js
@@ -1,50 +1,54 @@
-require('dotenv').config();
-const compression = require('compression')
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const { readdirSync } = require('fs')
-const connectDb = require('./Config/db')
+const compression = require("compression");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { readdirSync } = require("fs");
+const connectDb = require("./Config/db");
 const app = express();
-const path = require('path')
-const { checkOverdueTasks, updateOverdueTasks, resetDailyStreakStatus } = require('./job/cronJob');
-const mongoSanitize = require('express-mongo-sanitize')
+const path = require("path");
+const {
+  checkOverdueTasks,
+  updateOverdueTasks,
+  resetDailyStreakStatus,
+} = require("./job/cronJob");
+const mongoSanitize = require("express-mongo-sanitize");
 
-const imagesPath = path.join(__dirname, '../images/badges');
-
+const imagesPath = path.join(__dirname, "../images/badges");
+require("dotenv").config();
 connectDb();
-app.use(mongoSanitize())
-app.use(morgan('dev'));
-app.use(compression({
-  threshold: 0, 
-}));
 
-app.use(cors({
-  origin: [
-    'https://slimelist.netlify.app',
-    'http://localhost:5173'
-  ],
-  credentials: true,
-  methods: ['GET','POST','PUT','PATCH','DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['set-cookie']
-}));
+app.use(morgan("dev"));
+app.use(
+  compression({
+    threshold: 0,
+  })
+);
+
+app.use(
+  cors({
+    origin: ["https://slimelist.netlify.app", "http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["set-cookie"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
-
-app.use('/uploads', compression(), express.static(path.join(__dirname, 'uploads')));
-app.use('/images', compression(), express.static(imagesPath));
-
-
-require('dotenv').config();
+app.use(mongoSanitize());
+app.use(
+  "/uploads",
+  compression(),
+  express.static(path.join(__dirname, "uploads"))
+);
+app.use("/images", compression(), express.static(imagesPath));
 
 checkOverdueTasks();
 
+readdirSync("./Routes").map((route) =>
+  app.use("/api", require("./Routes/" + route))
+);
 
-
-readdirSync('./Routes').map((route) => 
-  app.use('/api',require('./Routes/' + route)))
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT,()=> console.log(`Server is running on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
